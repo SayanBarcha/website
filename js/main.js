88e76146a9835d5a90173969776fd6f6ff5b1200
Sayan Barcha <99346110+SayanBarcha@users.noreply.github.com>
2022-04-04 20:08:35 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load', function(){
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+    });
+});    
